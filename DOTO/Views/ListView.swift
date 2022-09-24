7de1deb85ec1495d6b6ca78626635454aea87ee8
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-24 22:07:28 +0200

    corrected ListView by implementing ItemModel into it

--- DOTO/Views/ListView.swift
+++ DOTO/Views/ListView.swift
@@ -19,8 +19,7 @@ struct ListView: View {
         List {
             
             ForEach(items) { item in
-                Text("Hi")
-               // ListRowView(title: item)
+                ListRowView(item: item)
             }
         }
         .listStyle(PlainListStyle())
