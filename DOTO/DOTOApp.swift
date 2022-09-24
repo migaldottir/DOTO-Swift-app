286fdcd43ecae95114f6cc37db44bb136cbedf4b
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-24 16:56:50 +0200

    Initial Commit

---
+++ DOTO/DOTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOTOApp.swift
+//  DOTO
+//
+//  Created by Refr Asta on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DOTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
