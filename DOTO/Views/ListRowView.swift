5153aa7e7093e77762e8c48de2d4060af34e0dba
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-24 21:58:37 +0200

    improved styling of the items previewed in ListRowView

--- DOTO/Views/ListRowView.swift
+++ DOTO/Views/ListRowView.swift
@@ -14,9 +14,12 @@ struct ListRowView: View {
     var body: some View {
         HStack{
             Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .orange)
             Text(item.title)
             Spacer()
         }
+        .font(.title2)
+        .padding(.vertical, 8)
     }
 }
 
