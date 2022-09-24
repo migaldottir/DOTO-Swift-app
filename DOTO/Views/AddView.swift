33d371f1353ac0ed834bbda905be0008d1b91bff
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-24 20:28:15 +0200

    in AddView: styled & added TextField, Title, Button

--- DOTO/Views/AddView.swift
+++ DOTO/Views/AddView.swift
@@ -6,22 +6,48 @@
 //
 
 import SwiftUI
+let backgroundGradient = LinearGradient(
+    colors: [Color.mint, Color.blue],
+    startPoint: .top, endPoint: .bottom)
+
 
 struct AddView: View {
     
     @State var textFieldText: String = ""
     var body: some View {
         ScrollView {
-            TextField("Type something here...", text: $textFieldText)
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(.white)
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                } )
+                .padding(14)
+            }
+                 .navigationTitle("Add an Item 🖍")
+            
         }
-        .navigationTitle("Add an Item 🖍")
+        .background(backgroundGradient)
+        
     }
-}
-
-struct AddView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            AddView()
+        
+    struct AddView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationView {
+                AddView()
+            }
         }
     }
 }
