fa8c750bead47d6869aef70b8286ce084bcafdb4
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-24 17:44:44 +0200

    deleted ContentView file, created Views group with two list files in them

--- DOTO/DOTOApp.swift
+++ DOTO/DOTOApp.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+/*
+ MVVM Architecture
+ 
+ Model - data point
+View - UI
+ ViewModel - manages Models for View
+ 
+ */
 @main
 struct DOTOApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ListView()
+            }
         }
     }
 }
