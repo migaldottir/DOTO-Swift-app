a99869b485dc5b945f1ccfc4d5ef59cc05389a06
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-25 17:34:21 +0200

    added secondary asset color to the assets and use it in button styling

--- DOTO/Views/NoItemsView.swift
+++ DOTO/Views/NoItemsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct NoItemsView: View {
     
     @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
@@ -26,13 +28,13 @@ struct NoItemsView: View {
                                        .font(.headline)
                                        .frame(height: 55)
                                        .frame(maxWidth: .infinity)
-                                       .background(animate ? Color.red : Color.accentColor)
+                                       .background(animate ? secondaryAccentColor : Color.accentColor)
                                        .cornerRadius(10)
                 })
                 .padding(.horizontal, animate ? 30 : 50)
                 .shadow(
-                    color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
-                    radius: animate ? 30 : 10,
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 25 : 10,
                     x: 0.0,
                     y: animate ? 50 : 30)
                 .scaleEffect(animate ? 1.1 : 1.0)
