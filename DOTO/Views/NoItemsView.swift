56232c51e28ad40b52c550154e633dca8377b557
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-25 16:55:33 +0200

    defined Stack within NoItemsView

--- DOTO/Views/NoItemsView.swift
+++ DOTO/Views/NoItemsView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct NoItemsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Quo vadis? Go add some new items to your doto list!")
+                NavigationLink(
+                    destination: AddView(),
+                               label: {
+                    Text("Add something! 🚣‍♀️")
+                                       .foregroundColor(.white)
+                                       .font(.headline)
+                                       .frame(height: 55)
+                                       .frame(maxWidth: .infinity)
+                                       .background(Color.accentColor)
+                                       .cornerRadius(10)
+                })
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+        }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
-}
+    
+    struct NoItemsView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationView {
+                NoItemsView()
+                    .navigationTitle("Title")
+            }
+            NoItemsView()
+        }
+    }
 
-struct NoItemsView_Previews: PreviewProvider {
-    static var previews: some View {
-        NoItemsView()
-    }
-}
